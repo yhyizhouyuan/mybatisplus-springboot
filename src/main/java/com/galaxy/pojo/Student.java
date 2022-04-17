a3d60fd79dc75953eb89fe9d45ebc7e2603fd78d
yhyizhouyuan <2017767351@qq.com>
2022-04-17 10:02:03 +0800

    测试提交pojo

---
+++ src/main/java/com/galaxy/pojo/Student.java
@@ -0,0 +1,17 @@
+package com.galaxy.pojo;
+
+import lombok.Data;
+
+/**
+ * <p>TODO</p>
+ *
+ * @author Hance
+ * @version V1.0.0
+ * @date 2022/4/17 10:00
+ */
+@Data
+public class Student {
+    private Integer id;
+    private String name;
+    private String password;
+}
